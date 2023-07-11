07189f4554dfad8afd432360d6b1546b40b8b179
CarlosHZB <carlosziliolibraga@hotmail.com>
2023-07-10 23:03:43 -0300

    Rooms screen almost finished

--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -1,15 +1,58 @@
-import { HeaderRooms, RoomsContainer, Squares, WrapSquaresAndDrops } from "../../styles/Rooms";
+import { CardRoomTitle, CardsTitles, DropDown, HeaderRooms, RoomsCards, RoomsCardsContainer, RoomsContainer, RoomsSquares, SquareTexts, Squares, Title, WrapDropDowns, WrapRooms, WrapSquares } from "../../styles/Rooms";
 
 export default function Rooms(){
     return(
         <RoomsContainer>
             <HeaderRooms>
-                <WrapSquaresAndDrops>
-                    <Squares/>
-                    {/* <Squares/> */}
-                    <div>Opa</div>
-                </WrapSquaresAndDrops>
+                <WrapSquares>
+                    <Squares color="#42BC37"/>
+                    <SquareTexts>Aberto</SquareTexts>
+                    <Squares color="#EF835F"/> 
+                    <SquareTexts>Fechado</SquareTexts>
+                </WrapSquares>
+                <WrapDropDowns>
+                    <DropDown width="200px">
+                        <option>Todos os blocos</option>
+                    </DropDown>
+                    <DropDown width="100px">
+                        <option>Todas</option>
+                    </DropDown>
+                </WrapDropDowns>
             </HeaderRooms>
+            <RoomsCardsContainer>
+                <Title>Salas</Title>
+                <RoomsCards>
+                    <CardsTitles>Bloco A</CardsTitles>
+                    <CardRoomTitle>Salas</CardRoomTitle>
+                    <WrapRooms>
+                        <RoomsSquares color="#42BC37">102</RoomsSquares>
+                        <RoomsSquares color="#EF835F">103</RoomsSquares>
+                        <RoomsSquares color="#42BC37">104</RoomsSquares>
+                        <RoomsSquares color="#EF835F">105</RoomsSquares>
+                    </WrapRooms>
+                </RoomsCards>
+                <RoomsCards>
+                    <CardsTitles>Bloco B</CardsTitles>
+                    <CardRoomTitle>Salas</CardRoomTitle>
+                    <WrapRooms>
+                        <RoomsSquares color="#42BC37">102</RoomsSquares>
+                        <RoomsSquares color="#EF835F">103</RoomsSquares>
+                        <RoomsSquares color="#42BC37">104</RoomsSquares>
+                        <RoomsSquares color="#EF835F">105</RoomsSquares>
+                    </WrapRooms>
+                </RoomsCards>
+                <RoomsCards>
+                    <CardsTitles>Bloco C</CardsTitles>
+                    <CardRoomTitle>Salas</CardRoomTitle>
+                    <WrapRooms>
+                        <RoomsSquares color="#42BC37">102</RoomsSquares>
+                        <RoomsSquares color="#EF835F">103</RoomsSquares>
+                        <RoomsSquares color="#42BC37">104</RoomsSquares>
+                        <RoomsSquares color="#EF835F">105</RoomsSquares>
+                    </WrapRooms>
+                </RoomsCards>
+                
+            </RoomsCardsContainer>
         </RoomsContainer>
     );
 }
--- src/styles/Rooms.ts
+++ src/styles/Rooms.ts
@@ -13,19 +13,131 @@ export const HeaderRooms = styled.div`
     flex-direction: row;
     height: 90px;
     background-color: #263238;
-    justify-content: space-evenly;
     align-items: center;
 `
 
-export const WrapSquaresAndDrops = styled.div`
+export const WrapSquares = styled.div`
     display: flex;
+    margin-right: auto;
     flex-direction: row;
+    align-items: center;
 `
 
-export const Squares = styled.div`
-    background-color: #42BC37;
-    height: 60px;
-    width: 60px;
-    border-radius: 7px;
+export const WrapDropDowns = styled.div`
+    display: flex;
+    margin-left: auto;
+    flex-direction: row;
+    align-items: center;
+`
+
+interface SquareProps {
+    color: string;
+}
+  
+export const Squares = styled.div<SquareProps>`
+   margin-left: 3rem;
+   background-color: ${(props) => props.color};
+   height: 35px;
+   width: 35px;
+   border-radius: 7px;
+   border: none;
+`
+
+export const SquareTexts = styled.h1`
+    color: white;
+    font-family: 'Montserrat';
+    font-size: 18px;
+    font-weight: 100;
+    margin-left: 0.5rem;
+`
+
+interface DropProps {
+    width: string;
+}
+
+export const DropDown = styled.select<DropProps>`
+    margin-right: 3rem;
+    background-color: #F2F8FD;
     border: none;
+    border-radius: 6px;
+    padding: 8px;
+    font-size: 16px;
+    text-align: center;
+    width: ${(props) => props.width};
+    font-family: 'Montserrat';
+    font-size: 18px;
+    font-weight: 100;
+
+    option {
+    &:hover {
+      background-color: #42BC37;
+      color: white;
+    }
+  }
+`
+
+export const RoomsCardsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 3rem;
+`
+
+export const Title = styled.h1`
+    color: #263238;
+    font-family: 'Montserrat';
+    font-size: 25px;
+    font-weight: 700;
+`
+
+export const RoomsCards = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #F2F8FD;
+    width: 95%;
+    height: 110px;
+    border-radius: 5px;
+    margin-bottom: 35px;
+`
+
+export const CardsTitles = styled.h1`
+    color: #263238;
+    font-family: 'Montserrat';
+    font-size: 15px;
+    font-weight: 700;
+    margin-left: 1rem;
+`
+
+export const CardRoomTitle = styled.h1`
+    margin-top: 0;
+    color: #263238;
+    font-family: 'Montserrat';
+    font-size: 12px;
+    font-weight: 500;
+    margin-left: 1rem;
+`
+
+export const WrapRooms = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+interface RoomSquareProps {
+    color: string;
+}
+
+export const RoomsSquares = styled.div<RoomSquareProps>`
+   display: flex;
+   margin-left: 1.2rem;
+   background-color: ${(props) => props.color};
+   height: 38px;
+   width: 38px;
+   border-radius: 5px;
+   border: none;
+   color: white;
+   align-items: center;
+   justify-content: center;
+   font-family: 'Montserrat';
+   font-size: 12px;
+   font-weight: 700;
 `
